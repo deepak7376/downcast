ceacc22316e39e57031bea34cf00b0d0e354f236
Deepak Yadav <dky.united@gmail.com>
2020-08-09 19:34:48 +0530

    v0.0.5

---
+++ src/downcast.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+def reduce(df):
+    cols = df.dtypes.index.tolist()
+    types = df.dtypes.values.tolist()
+    for i,t in enumerate(types):
+        if 'int' in str(t):
+            if df[cols[i]].min() > np.iinfo(np.int8).min and df[cols[i]].max() < np.iinfo(np.int8).max:
+                df[cols[i]] = df[cols[i]].astype(np.int8)
+            elif df[cols[i]].min() > np.iinfo(np.int16).min and df[cols[i]].max() < np.iinfo(np.int16).max:
+                df[cols[i]] = df[cols[i]].astype(np.int16)
+            elif df[cols[i]].min() > np.iinfo(np.int32).min and df[cols[i]].max() < np.iinfo(np.int32).max:
+                df[cols[i]] = df[cols[i]].astype(np.int32)
+            else:
+                df[cols[i]] = df[cols[i]].astype(np.int64)
+        elif 'float' in str(t):
+            if df[cols[i]].min() > np.finfo(np.float16).min and df[cols[i]].max() < np.finfo(np.float16).max:
+                df[cols[i]] = df[cols[i]].astype(np.float16)
+            elif df[cols[i]].min() > np.finfo(np.float32).min and df[cols[i]].max() < np.finfo(np.float32).max:
+                df[cols[i]] = df[cols[i]].astype(np.float32)
+            else:
+                df[cols[i]] = df[cols[i]].astype(np.float64)
+        elif t == np.object:
+            if cols[i] == 'date':
+                df[cols[i]] = pd.to_datetime(df[cols[i]], format='%Y-%m-%d')
+            else:
+                df[cols[i]] = df[cols[i]].astype('category')
+    return df
