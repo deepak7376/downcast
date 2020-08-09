fd3b70f7ec22468ae010ee8efd15b6daea642bbe
Deepak Yadav <dky.united@gmail.com>
2020-08-09 14:16:35 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Downcast", # Replace with your own username
+    version="0.0.1",
+    author="dky",
+    author_email="dky.united@gmail.com",
+    description="Reduce the pandas dataframe size automatically.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/deepak7376/downcast",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
